300ad44a44e82e8ffd484c5e3bbdf9683579f5f1
Lucas Queirolo dos Santos <lq.santos@hotmail.com>
2021-04-11 21:27:38 -0300

    Project File
    - Added the main project files

---
+++ Assets/Scripts/Player1.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+public class Player1 : MonoBehaviour
+{
+    public Player2 player2 = null;
+    public int playerIndex = 1;
+    public float speed = 5.0f;
+    public Rigidbody _rigidbody = null;
+    public float jumpForce = 1.0f;
+    public bool _jumping = false;
+    public GameObject _camera1 = null;
+    public GameObject _camera2 = null;
+
+    void Update()
+    {
+        if (playerIndex == 1)
+        {
+            Vector3 direction = Vector3.zero;
+            direction.x = Input.GetAxis("Horizontal");
+            transform.position += direction * (speed * Time.deltaTime);
+
+            if (Input.GetKeyUp(KeyCode.Alpha2))
+            {
+                ChangePlayer();
+                Debug.Log("MUDEI PRO PLAYER 2");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+    }
+
+    private void Jump()
+    {
+        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ChangePlayer()
+    {
+        player2.playerIndex = -1;
+        playerIndex = -1;
+        _camera1.SetActive(false);
+        _camera2.SetActive(true);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        // if (other.gameObject.CompareTag("Floor"))
+        // {
+        //     RaycastHit hit = Physics.Raycast(transform.localPosition, 0.5f, Vector3.down, 1.0f, 1 << 8);
+
+        //  if (hit.collider == other.collider)
+        //     {
+        //      _jumping = false;
+
+        //  }
+        // }
+    } //PROBLEMA
+}
---
+++ Assets/Scripts/Player2.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class Player2 : MonoBehaviour
+{
+    public Player1 player1 = null;
+    public int playerIndex = 1;
+    public float speed = 5.0f;
+    public Rigidbody _rigidbody = null;
+    public float jumpForce = 1.0f;
+    public bool _jumping = false;
+    public GameObject _camera1 = null;
+    public GameObject _camera2 = null;
+
+    void Update()
+    {
+        if (playerIndex == -1)
+        {
+            Vector3 direction = Vector3.zero;
+            direction.x = Input.GetAxis("Horizontal");
+            transform.position += direction * (speed * Time.deltaTime);
+           
+            if (Input.GetKeyUp(KeyCode.Alpha1))
+            {
+                ChangePlayer();
+                Debug.Log("MUDEI PRO PLAYER 1");
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Jump();
+            }
+        }
+    }
+    private void Jump()
+    {
+        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void ChangePlayer()
+    {
+        player1.playerIndex = 1;
+        playerIndex = 1;
+        _camera1.SetActive(true);
+        _camera2.SetActive(false);
+    }
+}
